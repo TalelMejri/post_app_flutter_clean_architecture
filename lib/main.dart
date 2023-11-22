b75177724372d6a08e42496fc7572761b85cf2d8
TalelMejri <talelmejri8@gmail.com>
2023-11-22 10:35:46 +0100

    postPages

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:post_app/features/post/presentation/bloc/posts/posts_bloc.dart';
+import 'package:post_app/features/post/presentation/pages/posts_pages.dart';
+
+import 'core/app_theme.dart';
+
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Bonjour',
+        theme: appTheme,
+        home: PostPage(),
+    );
+  }
+}
